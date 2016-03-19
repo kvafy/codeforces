17f04969c265b31a313baf44c1732593565e1ad8
David Chaloupka <chaloupka.david@gmail.com>
2016-03-19 13:13:09 +0100

    Modified Haskell template

--- template.hs
+++ template.hs
@@ -3,9 +3,13 @@ instructions go here...
 -}
 
 module Main where
+import Data.List (group, sort)
 
 getLines :: Int -> IO [String]
 getLines n = sequence $ replicate n getLine
+
+distinct :: (Ord a) => [a] -> [a]
+distinct = map head . group . sort
 
 main :: IO ()
 main = do
