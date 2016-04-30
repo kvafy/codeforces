4fa8f293ab373616acecfd8e18c80d8a9695a881
David Chaloupka <chaloupka.david@gmail.com>
2016-04-30 10:21:17 +0200

    Round #349 - fix for C.

--- 2016-04-29_Round-349-div-2/B.hs
+++ 2016-04-29_Round-349-div-2/B.hs
@@ -1,5 +1,45 @@
 {-
-instructions go here...
+As some of you know, cubism is a trend in art, where the problem of constructing
+volumetrical shape on a plane with a combination of three-dimensional geometric
+shapes comes to the fore.
+
+A famous sculptor Cicasso, whose self-portrait you can contemplate, hates cubism.
+He is more impressed by the idea to transmit two-dimensional objects through
+three-dimensional objects by using his magnificent sculptures. And his new
+project is connected with this. Cicasso wants to make a coat for the haters of
+anticubism. To do this, he wants to create a sculpture depicting a well-known
+geometric primitive — convex polygon.
+
+Cicasso prepared for this a few blanks, which are rods with integer lengths, and
+now he wants to bring them together. The i-th rod is a segment of length li.
+
+The sculptor plans to make a convex polygon with a nonzero area, using all rods
+he has as its sides. Each rod should be used as a side to its full length. It
+is forbidden to cut, break or bend rods. However, two sides may form a straight
+angle 180°.
+
+Cicasso knows that it is impossible to make a convex polygon with a nonzero area
+out of the rods with the lengths which he had chosen. Cicasso does not want to
+leave the unused rods, so the sculptor decides to make another rod-blank with
+an integer length so that his problem is solvable. Of course, he wants to make
+it as short as possible, because the materials are expensive, and it is improper
+deed to spend money for nothing.
+
+Help sculptor!
+
+Input
+The first line contains an integer n (3 ≤ n ≤ 105) — a number of rod-blanks.
+
+The second line contains n integers li (1 ≤ li ≤ 109) — lengths of rods, which
+Cicasso already has. It is guaranteed that it is impossible to make a polygon
+with n vertices and nonzero area using the rods Cicasso already has.
+
+Output
+Print the only integer z — the minimum length of the rod, so that after adding
+it it can be possible to construct convex polygon with (n + 1) vertices and
+nonzero area from all of the rods.
+
+Found at: http://codeforces.com/contest/667/problem/B
 -}
 
 module Main where
--- 2016-04-29_Round-349-div-2/C.hs
+++ 2016-04-29_Round-349-div-2/C.hs
@@ -1,13 +1,73 @@
 {-
-instructions go here...
+First-rate specialists graduate from Berland State Institute of Peace and
+Friendship. You are one of the most talented students in this university. The
+education is not easy because you need to have fundamental knowledge in different
+areas, which sometimes are not related to each other.
+
+For example, you should know linguistics very well. You learn a structure of
+Reberland language as foreign language. In this language words are constructed
+according to the following rules. First you need to choose the "root" of the
+word — some string which has more than 4 letters. Then several strings with the
+length 2 or 3 symbols are appended to this word. The only restriction — it is
+not allowed to append the same string twice in a row. All these strings are
+considered to be suffixes of the word (this time we use word "suffix" to
+describe a morpheme but not the few last characters of the string as you may
+used to).
+
+Here is one exercise that you have found in your task list. You are given the
+word s. Find all distinct strings with the length 2 or 3, which can be suffixes
+of this word according to the word constructing rules in Reberland language.
+
+Two strings are considered distinct if they have different length or there is
+a position in which corresponding characters do not match.
+
+Let's look at the example: the word abacabaca is given. This word can be
+obtained in the following ways:
+  * root(abacabaca)
+  * root(abacaba) ca
+  * root(abacab) aca
+  * root(abaca) ba ca
+Thus, the set of possible suffixes for this word is {aca, ba, ca}.
+
+Input
+The only line contains a string s (5 ≤ |s| ≤ 10^4) consisting of lowercase
+English letters.
+
+Output
+On the first line print integer k — a number of distinct possible suffixes.
+On the next k lines print suffixes.
+
+Print suffixes in lexicographical (alphabetical) order.
+
+Found at: http://codeforces.com/contest/667/problem/C
 -}
 
 module Main where
 import Data.List (group, sort, splitAt)
 import Data.Set (Set(..), empty, singleton, toList, insert, unions, member)
 
+-- guaranteed O(n) solution
 solve :: String -> [String]
-solve str = sort . map reverse . toList . fst . suffixes (empty,"",0,empty) $ reverse str
+solve str = sort . toList . nth3_3 . foldr f f0 $ (drop 5 str)
+  where f0 = ([True,False,False,False,False], "-----", empty)
+        f :: Char -> ([Bool],[Char],Set String) -> ([Bool],[Char],Set String)
+        f x0 (oks@[ok1,ok2,ok3,ok4,ok5], xs@[x1,x2,x3,x4,x5], suffixes) =
+          let ok02 = ok2 && ok5
+                  || ok2 && [x0,x1] /= [x2,x3]
+              ok03 = ok3 && ok5
+                  || ok3 && [x0,x1,x2] /= [x3,x4,x5]
+              suffixes' = unions [ suffixes
+                                 , if ok02 then singleton [x0,x1] else empty
+                                 , if ok03 then singleton [x0,x1,x2] else empty
+                                 ]
+          in (enqueue (ok02 || ok03) oks, enqueue x0 xs, suffixes')
+        nth3_3 (_,_,x) = x
+        enqueue x xs = x : init xs
+
+
+-- DFS style, too slow
+solve' :: String -> [String]
+solve' str = sort . map reverse . toList . fst . suffixes (empty,"",0,empty) $ reverse str
   where suffixes :: (Set String, String, Int, Set Int) -> String -> (Set String, Set Int)
         suffixes (seen,prev,pos,processed) str =
           if pos `member` processed
