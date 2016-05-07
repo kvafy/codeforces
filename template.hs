8a2e06382ce8362ba5f1f5fcbcaf624414898bbb
David Chaloupka <chaloupka.david@gmail.com>
2016-05-07 10:00:18 +0200

    Extending Haskell template

--- template.hs
+++ template.hs
@@ -11,6 +11,10 @@ getLines n = sequence $ replicate n getLine
 distinct :: (Ord a) => [a] -> [a]
 distinct = map head . group . sort
 
+shorterThan :: [a] -> Int -> Bool
+shorterThan [] n = n > 0
+shorterThan (_:xs) n = shorterThan xs (n-1)
+
 main :: IO ()
 main = do
   line <- getLine
