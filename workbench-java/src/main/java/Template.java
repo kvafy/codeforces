f82b584d1b3961c82ffb2a1730ec970dc32242f0
David Chaloupka <chaloupka.david@gmail.com>
2016-05-30 23:41:30 +0200

    Adjusting Java template - new FastReader I/O helper class

--- workbench-java/src/main/java/Template.java
+++ workbench-java/src/main/java/Template.java
@@ -1,61 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Template {
 
     public static void main(String[] args) throws IOException {
-        int[] nm = IOUtils.readIntArray(2);
+        FastReader.init(System.in);
+
+        int n = FastReader.readInt();
 
         //TODO solution here
-
     }
 
+    // inspired by https://www.cpe.ku.ac.th/~jim/java-io.html
+    static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
 
-    static class SafeUtils {
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
 
-        public static int get(int[] array, int idx, int defVal) {
-            if (0 <= idx && idx < array.length) {
-                return array[idx];
-            } else {
-                return defVal;
+        static String readString() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                String line = reader.readLine();
+                if (line == null) {
+                    throw new IllegalStateException("No more input");
+                }
+                tokenizer = new StringTokenizer(line);
             }
+            return tokenizer.nextToken();
         }
 
-        public static int get(int[][] matrix, int r, int c, int defVal) {
-            if (0 <= r && r < matrix.length && 0 <= c && c < matrix[r].length) {
-                return matrix[r][c];
-            } else {
-                return defVal;
-            }
+        static int readInt() throws IOException {
+            return Integer.parseInt(readString());
         }
 
-    }
-
-
-
-    static class IOUtils {
-        private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        public static String readLine() throws IOException {
-            return reader.readLine();
-        }
-
-        public static int readInt() throws IOException {
-            int c;
-            while ((c = reader.read()) != -1 && !Character.isDigit(c)) {}
-
-            int result = c - '0';
-            while ((c = reader.read()) != -1 && Character.isDigit(c)) {
-                result = result * 10 + c - '0';
-            }
-
-            return result;
-        }
-
-        public static int[] readIntArray(int length) throws IOException {
+        static int[] readIntArray(int length) throws IOException {
             int[] result = new int[length];
             for (int i = 0 ; i < length ; i++) {
                 result[i] = readInt();
@@ -63,7 +47,7 @@ public class Template {
             return result;
         }
 
-        public static int[][] readIntMatrix(int rows, int cols) throws IOException {
+        static int[][] readIntMatrix(int rows, int cols) throws IOException {
             int[][] matrix = new int[rows][];
             for (int i = 0 ; i < rows ; i++) {
                 matrix[i] = readIntArray(cols);
@@ -71,42 +55,12 @@ public class Template {
             return matrix;
         }
 
-        public static long readLong() throws IOException {
-            int c;
-            while ((c = reader.read()) != -1 && !Character.isDigit(c)) {}
-
-            long result = c - '0';
-            while ((c = reader.read()) != -1 && Character.isDigit(c)) {
-                result = result * 10 + c - '0';
-            }
-
-            return result;
+        static long readLong() throws IOException {
+            return Long.parseLong(readString());
         }
 
-        public static long[] readLongArray(int length) throws IOException {
-            long[] result = new long[length];
-            for (int i = 0 ; i < length ; i++) {
-                result[i] = readLong();
-            }
-            return result;
-        }
-
-        public static long[][] readLongMatrix(int rows, int cols) throws IOException {
-            long[][] matrix = new long[rows][];
-            for (int i = 0 ; i < rows ; i++)
-                matrix[i] = readLongArray(cols);
-            return matrix;
-        }
-
-        public static void print(int[] array, String sep) {
-            String s = "";
-            for (int item : array) {
-                System.out.print(s);
-                s = sep;
-                System.out.print(item);
-            }
-            System.out.println();
+        static double readDouble() throws IOException {
+            return Double.parseDouble(readString());
         }
     }
-
 }
